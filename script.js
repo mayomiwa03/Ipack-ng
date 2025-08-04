bd9b2f78ff50b33fb98b908926c016e0ef047d76
Daniel Adeagbo <oluwamayomiwa03@gmail.com>
2025-08-04 20:52:29 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// Hamburger menu toggle
+("use strict");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-list");
+const navItems = document.querySelectorAll(".nav-list li"); // Select all list items
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+// Close nav when clicking outside list items
+document.addEventListener("click", (event) => {
+  const isClickOnHamburger = hamburger.contains(event.target);
+  const isClickOnNavItem = Array.from(navItems).some((item) =>
+    item.contains(event.target)
+  );
+
+  if (!isClickOnHamburger && !isClickOnNavItem) {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }
+});
